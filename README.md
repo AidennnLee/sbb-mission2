82e2d08089328598bea213b2e60b7ec560596603
Aidennn-Lee <yw010804@gmail.com>
2023-11-29 16:47:38 +0900

    2-4

--- README.md
+++ README.md
@@ -8,3 +8,23 @@ Github 링크는 [다음](https://github.com/AidennnLee/sbb-mission2.git)
 
 - HelloController : 환영 페이지
 - MainController : SBB 환영 페이지
+
+### Entity
+
+#### 질문(Question) 엔티티
+
+|속성명|	설명|
+|--|--|
+|id|	질문의 고유 번호|
+|subject|	질문의 제목|
+|content|	질문의 내용|
+|create_date|	질문을 작성한 일시|
+
+#### 답변(Answer) 엔티티
+
+|속성명| 	설명         |
+|--|-------------|
+|id| 	답변의 고유 번호  |
+|question| 	질문         |
+|content| 	답변의 내용     |
+|create_date| 	답변을 작성한 일시 |
---
+++ src/main/java/com/mysite/sbb2/Question.java
@@ -0,0 +1,29 @@
+package com.mysite.sbb2;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id //PK로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //@GeneratedValue : 데이터 저장 시 1씩 자동 증가. GenerationType.IDENTITY : 해당 컬럼만의 독립적인 시퀀스를 생성
+    private Integer id; //기본 타입이 아닌 객체를 이용하는 이유 : 먼저 생성 후 값을 넣어주는데, 이 때 null로 둘 수 있기 때문.
+
+    @Column(length = 200) //제목은 200자로 제한함
+    private String subject;
+
+    @Column(columnDefinition = "TEXT") //내용은 글자수 제한하지 않음.
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) //CascadeType.REMOVE : 질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위함
+    private List<Answer> answerList;
+}
