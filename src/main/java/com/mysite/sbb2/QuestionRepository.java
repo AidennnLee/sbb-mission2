9c5ff31c7ba05f23fb807971d2d7ecf9323bf60d
Aidennn-Lee <yw010804@gmail.com>
2023-11-30 11:55:34 +0900

    2-5

---
+++ src/main/java/com/mysite/sbb2/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.mysite.sbb2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//JpaRepository를 상속할 때는 제네릭스 타입으로 리포지터리의 대상이 되는 엔티티의 타입과 해당 엔티티의 PK의 속성 타입을 지정해야 한다.
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+}
